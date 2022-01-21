0c850423e9200c58cf7cc39cb3cab5a8ee6a62bf
김세준 <93368832+dailyzett@users.noreply.github.com>
2022-01-21 16:54:35 +0900

    10. 가장 짧은 문자거리

--- src/ProofNumber.java
+++ src/ProofNumber.java
@@ -2,17 +2,38 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class ProofNumber {
-    public int solution(String input){
-        String s = input.toLowerCase().replaceAll("[a-z]","");
-        int a =  Integer.parseInt(s);
-        return a;
+    public int[] solution(String input1, char input2) {
+        int[] answer = new int[input1.length()];
+        int p = 1000;
+
+        for(int i = 0; i < input1.length(); i++){
+            if(input1.charAt(i) == input2){
+                p = 0;
+            }else{
+                p++;
+            }
+            answer[i] = p;
+        }
+
+        for(int i = input1.length() - 1; i >= 0; i--){
+            if(input1.charAt(i) == input2) p = 0;
+            else{
+                p++;
+                answer[i] = Math.min(answer[i], p);
+            }
+        }
+
+        return answer;
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
+        String input1 = in.next();
+        char input2 = in.next().charAt(0);
 
-        ProofNumber app = new ProofNumber();
-        System.out.println(app.solution(input));
+        ProofNumber proofNumber = new ProofNumber();
+        for(int x : proofNumber.solution(input1, input2)){
+            System.out.print(x + " ");
+        }
     }
 }
