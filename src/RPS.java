fb6531b34e18d9fb99c8ceff5f2b11526b4740d0
김세준 <93368832+dailyzett@users.noreply.github.com>
2022-01-24 10:53:09 +0900

    2. Array - 3. 가위바위보

---
+++ src/RPS.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+/*가위바위보
+ * */
+public class RPS {
+
+    public char[] solution(int num, int[] aInfo, int[] bInfo) {
+        char[] answer = new char[num];
+
+        for (int i = 0; i < num; i++) {
+            if (Math.abs(aInfo[i] - bInfo[i]) == 1) {
+                if (aInfo[i] > bInfo[i]) {
+                    answer[i] = 'A';
+                } else if (aInfo[i] < bInfo[i]) {
+                    answer[i] = 'B';
+                }
+            } else if (aInfo[i] - bInfo[i] == 0) {
+                answer[i] = 'D';
+            } else {
+                if (aInfo[i] > bInfo[i]) {
+                    answer[i] = 'B';
+                } else if (aInfo[i] < bInfo[i]) {
+                    answer[i] = 'A';
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int[] aInfo = new int[num];
+        int[] bInfo = new int[num];
+
+        for (int i = 0; i < aInfo.length; i++) {
+            aInfo[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < bInfo.length; i++) {
+            bInfo[i] = sc.nextInt();
+        }
+
+        RPS app = new RPS();
+        for (char x : app.solution(num, aInfo, bInfo)) {
+            System.out.println(x);
+        }
+    }
+}
