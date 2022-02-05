ac48abbcb84d6f0dc105088885ccd3e8b4ebe847
dailyzett <dailyzett@gmail.com>
2022-02-05 13:14:25 +0900

    3. Two Pointers - 3. 최대 매출(Sliding window)

---
+++ src/inflearn/MaximumSales_3_3.java
@@ -0,0 +1,33 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class MaximumSales_3_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] nArray = new int[n];
+        for (int i = 0; i < n; i++) {
+            nArray[i] = sc.nextInt();
+        }
+
+        MaximumSales_3_3 app = new MaximumSales_3_3();
+        System.out.println(app.solution(n, k, nArray));
+    }
+
+    public int solution(int n, int k, int[] nArray) {
+        int answer = 0, sum = 0;
+
+        for (int i = 0; i < k; i++) {
+            sum += nArray[i];
+        }
+        answer = sum;
+        for (int i = k; i < n; i++) {
+            sum += (nArray[i] - nArray[i - k]);
+            answer = Math.max(answer, sum);
+        }
+        return answer;
+    }
+}
