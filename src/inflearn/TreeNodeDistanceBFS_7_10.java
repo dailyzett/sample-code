77e12caf2c613a69a2ec6c9d0168d75febe0d749
dailyzett <dailyzett@gmail.com>
2022-02-07 18:36:58 +0900

    7. Recursive, Tree, Graph - 9. Tree 말단 노드까지의 가장 짧은 경로(BFS)

---
+++ src/inflearn/TreeNodeDistanceBFS_7_10.java
@@ -0,0 +1,36 @@
+package inflearn;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TreeNodeDistanceBFS_7_10 {
+    Node root;
+
+    private int BFS(Node root) {
+        Queue<Node> Q = new LinkedList<>();
+        Q.offer(root);
+        int L = 0;
+
+        while (!Q.isEmpty()) {
+            int len = Q.size();
+            for (int i = 0; i < len; i++) {
+                Node current = Q.poll();
+                if (current.lt == null && current.rt == null) return L;
+                if (current.lt != null) Q.offer(current.lt);
+                if (current.rt != null) Q.offer(current.rt);
+            }
+            L++;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        TreeNodeDistanceBFS_7_10 tree = new TreeNodeDistanceBFS_7_10();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        System.out.println(tree.BFS(tree.root));
+    }
+}
