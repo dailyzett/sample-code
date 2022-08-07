729ca3577ae6e719aa17dfd2ffcf68c05d3bfb94
dailyzett <dailyzett@gmail.com>
2022-08-07 20:06:33 +0900

    필요없는 import 삭제

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -15,7 +15,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Controller
