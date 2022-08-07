6ce55a687dc6f6728e41cd7752d37665c0bff207
dailyzett <dailyzett@gmail.com>
2022-08-08 08:02:19 +0900

    멤버 조회 메서드 추가

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop.api;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.validation.constraints.NotEmpty;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
@@ -7,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,6 +21,36 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberApiController {
 
 	private final MemberService memberService;
+
+	@GetMapping("api/v1/members")
+	public List<Member> membersV1() {
+		return memberService.findMembers();
+	}
+
+	@GetMapping("api/v2/members")
+	public Result membersV2() {
+		List<Member> members = memberService.findMembers();
+		List<MemberDto> collect = members.stream()
+			.map(m -> new MemberDto(m.getName()))
+			.collect(Collectors.toList());
+
+		return new Result(collect.size(), collect);
+	}
+
+	@Data
+	@AllArgsConstructor
+	static class Result<T> {
+
+		private int count;
+		private T data;
+	}
+
+	@Data
+	@AllArgsConstructor
+	static class MemberDto {
+
+		private String name;
+	}
 
 	@PostMapping("/api/v1/members")
 	public CreateMemberResponse saveMemberV1(@RequestBody @Validated Member member) {
