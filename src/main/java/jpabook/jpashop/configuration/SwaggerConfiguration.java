f841022f5375c8fa82062ae64748deb8a295758d
dailyzett <dailyzett@gmail.com>
2022-08-07 20:10:43 +0900

    필요없는 종속성 제거

--- src/main/java/jpabook/jpashop/configuration/SwaggerConfiguration.java
+++ src/main/java/jpabook/jpashop/configuration/SwaggerConfiguration.java
@@ -1,14 +1,11 @@
 package jpabook.jpashop.configuration;
 
-import java.util.ArrayList;
 import jpabook.jpashop.controller.MemberController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
