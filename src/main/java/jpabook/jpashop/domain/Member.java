c5a8f59ea043b9dfb5981ce9349593109a8efb75
dailyzett <dailyzett@gmail.com>
2022-08-09 19:43:34 +0900

    @JsonIgnore 어노테이션 추가
    - jackson 라이브러리의 엔티티 순환 참조 방지

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
@@ -34,6 +35,7 @@ public class Member {
 	@Embedded
 	private Address address;
 
+	@JsonIgnore
 	@Builder.Default
 	@OneToMany(mappedBy = "member")
 	private List<Order> orders = new ArrayList<>();
