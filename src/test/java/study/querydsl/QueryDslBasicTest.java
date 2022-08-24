e186705a0c900e9f0304db59bfd8c35bd6065dfa
dailyzett <dailyzett@gmail.com>
2022-08-24 16:37:54 +0900

    QueryDSL 작동 테스트

---
+++ src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -0,0 +1,11 @@
+package study.querydsl;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class QueryDslBasicTest {
+
+
+}
