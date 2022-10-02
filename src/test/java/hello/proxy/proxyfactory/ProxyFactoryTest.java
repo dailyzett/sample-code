cd8c2e78618d8bdaa0401c89e7ca128fb2690061
dailyzett <dailyzett@gmail.com>
2022-10-02 21:14:52 +0900

    프록시 팩토리 빈 - 어드바이저 학습 테스트 코드 작성

--- src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
+++ src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -1,12 +1,12 @@
 package hello.proxy.proxyfactory;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ConcreteService;
 import hello.proxy.common.service.ServiceImpl;
 import hello.proxy.common.service.ServiceInterface;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.framework.ProxyFactory;
@@ -31,5 +31,24 @@ public class ProxyFactoryTest {
 		assertThat(AopUtils.isAopProxy(proxy)).isTrue();
 		assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
 		assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+	}
+
+	@Test
+	@DisplayName("구체 클래스만 있으면 CGLIB 사용")
+	void concreteProxy() {
+		ConcreteService target = new ConcreteService();
+		ProxyFactory proxyFactory = new ProxyFactory(target);
+		proxyFactory.setProxyTargetClass(true);
+		proxyFactory.addAdvice(new TimeAdvice());
+		ConcreteService proxy = (ConcreteService) proxyFactory.getProxy();
+
+		log.info("targetClass={}", target.getClass());
+		log.info("proxyClass={}", proxy.getClass());
+
+		proxy.call();
+
+		assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+		assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+		assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
 	}
 }
