413d777a5e89a3541291bdc4aa1ed59eb4f48236
dailyzett <dailyzett@gmail.com>
2022-10-18 23:35:46 +0900

    정적인 유틸리티 클래스 없애기: 최상위 함수와 프로퍼티

---
+++ src/test/kotlin/com/begin/three/ThreeOne.kt
@@ -0,0 +1,44 @@
+package com.begin.three
+
+import org.junit.jupiter.api.Test
+import strings.UNIX_LINE_SEPARATOR
+import strings.joinToString
+import strings.performOperation
+import strings.reportOperationCount
+
+class ThreeOne {
+    @Test
+    fun test1() {
+        val list = arrayListOf(1, 7, 53)
+        val map = hashMapOf(1 to "one", 7 to "seven", 53 to "fifty-three")
+        val set = hashSetOf(1, 7, 53)
+
+        println(set.javaClass)
+    }
+
+    @Test
+    fun test2() {
+        val strings = listOf("first", "second", "fourteenth")
+        println(strings.last())
+    }
+
+    @Test
+    fun test3() {
+        val list = listOf(1, 2, 3)
+        println(list)
+    }
+
+    @Test
+    fun test4() {
+        val list = listOf(1, 2, 3, 4)
+        println(joinToString(list, ", ", "", ""))
+        println(joinToString(list, postfix = ";", prefix = "# "))
+    }
+
+    @Test
+    fun test5() {
+        performOperation()
+        reportOperationCount()
+        println("Hello! $UNIX_LINE_SEPARATOR World")
+    }
+}
