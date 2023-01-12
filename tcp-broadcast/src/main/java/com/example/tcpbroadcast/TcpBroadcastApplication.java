d323497574f13105ccc84f68629660f327b06e9d
dailyzett <dailyzett@gmail.com>
2023-01-13 00:05:56 +0900

    TCP BroadCast
    컨트롤러에서 받은 메시지를 연결된 모든 TCP 클라이언트에 브로드캐스팅하는 코드

--- tcp-broadcast/src/main/java/com/example/tcpbroadcast/TcpBroadcastApplication.java
+++ tcp-broadcast/src/main/java/com/example/tcpbroadcast/TcpBroadcastApplication.java
@@ -2,11 +2,38 @@ package com.example.tcpbroadcast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.integration.ip.tcp.serializer.ByteArrayCrLfSerializer;
+
+import javax.net.SocketFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
 
 @SpringBootApplication
 public class TcpBroadcastApplication {
 
     private static final int PORT = 1234;
+
+    public static class Client implements Runnable {
+
+        private static final ByteArrayCrLfSerializer deserializer = new ByteArrayCrLfSerializer();
+
+        private static int next;
+
+        private final int instance = ++next;
+        @Override
+        public void run() {
+            try (Socket socket = SocketFactory.getDefault().createSocket("localhost", PORT)) {
+                socket.getOutputStream().write("hello\r\n".getBytes());
+                InputStream is = socket.getInputStream();
+                while (true) {
+                    System.out.println(new String(deserializer.deserialize(is)) + " from client# " + instance);
+                }
+            } catch (IOException ignored) {
+            }
+        }
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(TcpBroadcastApplication.class, args);
     }
--- tcp-broadcast/src/main/java/com/example/tcpbroadcast/configuration/Config.java
+++ tcp-broadcast/src/main/java/com/example/tcpbroadcast/configuration/Config.java
@@ -1,11 +1,23 @@
 package com.example.tcpbroadcast.configuration;
 
+import com.example.tcpbroadcast.service.BroadCaster;
+import com.example.tcpbroadcast.service.Sender;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.ip.dsl.Tcp;
 import org.springframework.integration.ip.tcp.connection.AbstractServerConnectionFactory;
+import org.springframework.integration.ip.tcp.connection.TcpConnectionServerListeningEvent;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+import static com.example.tcpbroadcast.TcpBroadcastApplication.Client;
 
 @Configuration
 public class Config {
@@ -16,5 +28,47 @@ public class Config {
     @Bean
     public AbstractServerConnectionFactory serverFactory() {
         return Tcp.netServer(PORT).get();
+    }
+
+    @Bean
+    public IntegrationFlow tcpServer(AbstractServerConnectionFactory serverFactory) {
+        return IntegrationFlow.from(Tcp.inboundAdapter(serverFactory))
+                .transform(p -> "connected!")
+                .channel("toTcp.input")
+                .get();
+    }
+
+    @Bean
+    public IntegrationFlow gateway() {
+        return IntegrationFlow.from(Sender.class)
+                .channel("toTcp.input")
+                .get();
+    }
+
+    @Bean
+    public IntegrationFlow toTcp(AbstractServerConnectionFactory serverFactory) {
+        return f -> f.handle(Tcp.outboundAdapter(serverFactory));
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor exec() {
+        ThreadPoolTaskExecutor exec = new ThreadPoolTaskExecutor();
+        exec.setCorePoolSize(50);
+        return exec;
+    }
+
+    @Bean
+    public ApplicationRunner runner(TaskExecutor exec, BroadCaster broadCaster) {
+        return args -> {
+            if (!this.listenLatch.await(10, TimeUnit.SECONDS)) {
+                throw new IllegalArgumentException("Failed to start listening");
+            }
+            IntStream.range(1, 20).forEach(i -> exec.execute(new Client()));
+        };
+    }
+
+    @EventListener
+    public void serverStarted(TcpConnectionServerListeningEvent event) {
+        this.listenLatch.countDown();
     }
 }
