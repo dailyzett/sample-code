e800c5f59b14952c7e9289335284047e0fc37171
dailyzett <dailyzett@gmail.com>
2023-03-07 01:59:47 +0900

    initial commit

--- src/main/kotlin/deepboot/deep/controller/HelloController.kt
+++ src/main/kotlin/deepboot/deep/controller/HelloController.kt
@@ -1,4 +1,13 @@
 package deepboot.deep.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class HelloController {
+
+    @GetMapping("/")
+    fun hello(): String {
+        return "Hello World!"
+    }
 }
