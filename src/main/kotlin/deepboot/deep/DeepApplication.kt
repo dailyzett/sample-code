a35329d0528e18836ee55e45cd6333f43ae7d0fb
dailyzett <dailyzett@gmail.com>
2023-03-10 23:57:49 +0900

    implement front controller

--- src/main/kotlin/deepboot/deep/DeepApplication.kt
+++ src/main/kotlin/deepboot/deep/DeepApplication.kt
@@ -3,6 +3,7 @@ package deepboot.deep
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.servlet.ServletContextInitializer
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import javax.servlet.http.HttpServlet
@@ -12,19 +13,23 @@ import javax.servlet.http.HttpServletResponse
 class DeepApplication
 
 fun main(args: Array<String>) {
-	val serverFactory = TomcatServletWebServerFactory()
-	val webServer = serverFactory.getWebServer(ServletContextInitializer{
-		it.addServlet("hello", object : HttpServlet() {
-			override fun service(req: HttpServletRequest?, resp: HttpServletResponse?) {
-				resp?.apply {
-					val name = req?.getParameter("name") ?: "default"
-					resp.status = HttpStatus.OK.value()
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE)
-					resp.writer.println("Hello $name")
-				}
-			}
-		}).addMapping("/hello")
-	})
+    val serverFactory = TomcatServletWebServerFactory()
+    val webServer = serverFactory.getWebServer(ServletContextInitializer {
+        it.addServlet("frontController", object : HttpServlet() {
+            override fun service(req: HttpServletRequest?, resp: HttpServletResponse?) {
 
-	webServer.start()
+				if (req?.requestURI == "/hello" && req.method == HttpMethod.GET.name) {
+					resp?.apply {
+						val name = req.getParameter("name") ?: "default"
+						resp.status = HttpStatus.OK.value()
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE)
+						resp.writer.println("Hello $name")
+					}
+				}
+
+            }
+        }).addMapping("/*")
+    })
+
+    webServer.start()
 }
