f02ee57cfd7a314e7993bb89a5ee569ff159016a
dailyzett <dailyzett@gmail.com>
2023-03-13 23:23:27 +0900

    restore initial spring boot application (runApplication)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,19 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.9"
-	id("io.spring.dependency-management") version "1.0.15.RELEASE"
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
+    id("org.springframework.boot") version "2.7.9"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.noarg") version "1.6.21"
+}
+
+noArg {
+    annotation("org.springframework.stereotype.Component")
+}
+
+allOpen {
+    annotation("org.springframework.stereotype.Component")
 }
 
 group = "deepboot"
@@ -12,23 +21,23 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- src/main/kotlin/deepboot/deep/DeepApplication.kt
+++ src/main/kotlin/deepboot/deep/DeepApplication.kt
@@ -1,12 +1,10 @@
 package deepboot.deep
 
+import org.springframework.boot.runApplication
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
-import org.springframework.boot.web.servlet.ServletContextInitializer
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext
 import org.springframework.web.servlet.DispatcherServlet
 
 
@@ -21,30 +19,7 @@ class DeepApplication {
     fun dispatcherServlet() = DispatcherServlet()
 }
 
-fun main() {
-
-    val applicationContext = object : AnnotationConfigWebApplicationContext() {
-
-        @Suppress("ACCIDENTAL_OVERRIDE")
-        override fun setClassLoader(classLoader: ClassLoader) {
-            super.setClassLoader(classLoader)
-        }
-
-        override fun onRefresh() {
-            super.onRefresh()
-
-            val serverFactory = this.getBean(ServletWebServerFactory::class.java)
-            val dispatcherServlet = this.getBean(DispatcherServlet::class.java)
-
-            val webServer = serverFactory.getWebServer(ServletContextInitializer {
-                it.addServlet("dispatcherServlet", DispatcherServlet(this))
-                    .addMapping("/*")
-            })
-            webServer.start()
-        }
-    }
-
-    applicationContext.register(DeepApplication::class.java)
-    applicationContext.refresh()
+fun main(args: Array<String>) {
+    runApplication<DeepApplication>(*args)
 }
 
