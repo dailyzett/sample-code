99fab216767eaa89c86002251f76c7339889783f
dailyzett <dailyzett@gmail.com>
2023-03-20 23:02:36 +0900

    add @Conditional annotation to run WAS that meets specific conditions

--- src/main/kotlin/deepboot/deep/config/autoconfig/JettyWebServerConfig.kt
+++ src/main/kotlin/deepboot/deep/config/autoconfig/JettyWebServerConfig.kt
@@ -3,9 +3,22 @@ package deepboot.deep.config.autoconfig
 import deepboot.deep.annotation.MyAutoConfiguration
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Condition
+import org.springframework.context.annotation.ConditionContext
+import org.springframework.context.annotation.Conditional
+import org.springframework.core.type.AnnotatedTypeMetadata
 
 @MyAutoConfiguration
+@Conditional(JettyWebServerConfig.Companion.JettyCondition::class)
 class JettyWebServerConfig {
     @Bean("jettyWebServerFactory")
     fun servletWebServerFactory() = JettyServletWebServerFactory()
+
+    companion object {
+        class JettyCondition : Condition {
+            override fun matches(context: ConditionContext, metadata: AnnotatedTypeMetadata): Boolean {
+                return true
+            }
+        }
+    }
 }
--- src/main/kotlin/deepboot/deep/config/autoconfig/TomcatWebServerConfig.kt
+++ src/main/kotlin/deepboot/deep/config/autoconfig/TomcatWebServerConfig.kt
@@ -3,9 +3,22 @@ package deepboot.deep.config.autoconfig
 import deepboot.deep.annotation.MyAutoConfiguration
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Condition
+import org.springframework.context.annotation.ConditionContext
+import org.springframework.context.annotation.Conditional
+import org.springframework.core.type.AnnotatedTypeMetadata
 
 @MyAutoConfiguration
+@Conditional(TomcatWebServerConfig.Companion.TomcatCondition::class)
 class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     fun servletWebServerFactory() = TomcatServletWebServerFactory()
+
+    companion object {
+        class TomcatCondition : Condition {
+            override fun matches(context: ConditionContext, metadata: AnnotatedTypeMetadata): Boolean {
+                return false
+            }
+        }
+    }
 }
