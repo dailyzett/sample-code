14a69318b71eb5fcdf11b48ef9c6a7a2649aeefc
dailyzett <dailyzett@gmail.com>
2023-03-21 22:44:48 +0900

    Consolidate two similar annotations into a single annotation for improved code readability and maintainability

--- src/main/kotlin/deepboot/deep/config/autoconfig/JettyWebServerConfig.kt
+++ src/main/kotlin/deepboot/deep/config/autoconfig/JettyWebServerConfig.kt
@@ -1,28 +1,13 @@
 package deepboot.deep.config.autoconfig
 
+import deepboot.deep.annotation.ConditionalMyOnClass
 import deepboot.deep.annotation.MyAutoConfiguration
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Condition
-import org.springframework.context.annotation.ConditionContext
-import org.springframework.context.annotation.Conditional
-import org.springframework.core.type.AnnotatedTypeMetadata
-import org.springframework.util.ClassUtils
 
 @MyAutoConfiguration
-@Conditional(JettyWebServerConfig.Companion.JettyCondition::class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 class JettyWebServerConfig {
     @Bean("jettyWebServerFactory")
     fun servletWebServerFactory() = JettyServletWebServerFactory()
-
-    companion object {
-        class JettyCondition : Condition {
-            override fun matches(context: ConditionContext, metadata: AnnotatedTypeMetadata): Boolean {
-                return ClassUtils.isPresent(
-                    "org.eclipse.jetty.server.Server",
-                    context.classLoader
-                )
-            }
-        }
-    }
 }
--- src/main/kotlin/deepboot/deep/config/autoconfig/TomcatWebServerConfig.kt
+++ src/main/kotlin/deepboot/deep/config/autoconfig/TomcatWebServerConfig.kt
@@ -1,28 +1,13 @@
 package deepboot.deep.config.autoconfig
 
+import deepboot.deep.annotation.ConditionalMyOnClass
 import deepboot.deep.annotation.MyAutoConfiguration
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Condition
-import org.springframework.context.annotation.ConditionContext
-import org.springframework.context.annotation.Conditional
-import org.springframework.core.type.AnnotatedTypeMetadata
-import org.springframework.util.ClassUtils
 
 @MyAutoConfiguration
-@Conditional(TomcatWebServerConfig.Companion.TomcatCondition::class)
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
     fun servletWebServerFactory() = TomcatServletWebServerFactory()
-
-    companion object {
-        class TomcatCondition : Condition {
-            override fun matches(context: ConditionContext, metadata: AnnotatedTypeMetadata): Boolean {
-                return ClassUtils.isPresent(
-                    "org.apache.catalina.startup.Tomcat",
-                    context.classLoader
-                )
-            }
-        }
-    }
 }
