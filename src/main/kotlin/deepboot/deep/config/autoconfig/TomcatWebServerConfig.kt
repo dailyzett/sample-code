114dad24c275d7a366ad0180793ccb7d10e0acc2
dailyzett <dailyzett@gmail.com>
2023-03-24 00:33:29 +0900

    properties 파일로 전환

--- src/main/kotlin/deepboot/deep/config/autoconfig/TomcatWebServerConfig.kt
+++ src/main/kotlin/deepboot/deep/config/autoconfig/TomcatWebServerConfig.kt
@@ -2,17 +2,26 @@ package deepboot.deep.config.autoconfig
 
 import deepboot.deep.annotation.ConditionalMyOnClass
 import deepboot.deep.annotation.MyAutoConfiguration
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 import org.springframework.context.annotation.Bean
+import org.springframework.core.env.Environment
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
-class TomcatWebServerConfig {
+class TomcatWebServerConfig() {
+
+    @Value(value = "\${context.path}")
+    lateinit var contextPath: String
+
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    fun servletWebServerFactory(): ServletWebServerFactory {
-        return TomcatServletWebServerFactory()
+    fun servletWebServerFactory(env: Environment): ServletWebServerFactory {
+        val factory = TomcatServletWebServerFactory()
+        println("contextPath: $contextPath")
+        factory.contextPath = this.contextPath
+        return factory
     }
 }
