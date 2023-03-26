ed6a6c3e002bb6670bb16dc36f151025ddb3f06e
dailyzett <dailyzett@gmail.com>
2023-03-26 20:42:36 +0900

    No converter found capable of converting from type [java.lang.String] to type [deepboot.config.autoconfig.MyDataSourceProperties] 에러 발생으로 인해 @SpringBootTest 로 잠시 변환

--- src/test/kotlin/deepboot/deep/DataSourceTest.kt
+++ src/test/kotlin/deepboot/deep/DataSourceTest.kt
@@ -1,14 +1,13 @@
 package deepboot.deep
 
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.TestPropertySource
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import javax.sql.DataSource
 
-@ExtendWith(SpringExtension::class)
+@SpringBootTest
 @ContextConfiguration(classes = [DeepApplication::class])
 @TestPropertySource("classpath:/application.yml")
 class DataSourceTest {
