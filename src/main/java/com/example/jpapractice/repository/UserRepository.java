9599c4f78d26f6c98abcdbb3fc492fa29121d986
sejun <dailyzett@gmail.com>
2023-07-19 23:00:05 +0900

    1:1 연관 매핑
    CQRS 모델을 사용함으로써 조회를 따로 분리한다.
    객체의 탐색이 쉬워진다고 연관관계를 남용하는 것은 좋지 않다.

--- src/main/java/com/example/jpapractice/repository/UserRepository.java
+++ src/main/java/com/example/jpapractice/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.example.jpapractice.repository;
 
+import com.example.jpapractice.entity.MembershipCard;
 import com.example.jpapractice.entity.User;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Repository
@@ -14,15 +16,31 @@ public class UserRepository {
 
     private final EntityManager em;
 
-    @Transactional
     public void save() {
         User user = new User("ssu30@inavi.kr", "수현", LocalDateTime.now());
         em.persist(user);
     }
 
-    @Transactional
     public String findName() {
         User user = em.find(User.class, "ssu30@inavi.kr");
         return user.getName();
+    }
+
+    public void saveCard() {
+        User user = em.find(User.class, "ssu30@inavi.kr");
+
+        MembershipCard card = MembershipCard.builder()
+                .cardNumber("8888111133332222")
+                .owner(user)
+                .expiryDate(LocalDate.of(2024, 12, 31))
+                .build();
+
+        em.persist(card);
+        em.flush();
+    }
+
+    public String findCardNumber() {
+        MembershipCard membershipCard = em.find(MembershipCard.class, "8888111133332222");
+        return membershipCard.getCardNumber();
     }
 }
--- src/test/java/com/example/jpapractice/repository/UserRepositoryTest.java
+++ src/test/java/com/example/jpapractice/repository/UserRepositoryTest.java
@@ -2,17 +2,22 @@ package com.example.jpapractice.repository;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Transactional
 class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
     @Test
+    @Commit
     void save() {
         userRepository.save();
     }
@@ -21,5 +26,17 @@ class UserRepositoryTest {
     void find() {
         String name = userRepository.findName();
         assertThat(name).isEqualTo("수현");
+    }
+
+    @Test
+    @Commit
+    void saveCard() {
+        userRepository.saveCard();
+    }
+
+    @Test
+    void findCard() {
+        userRepository.findCardNumber();
+        System.out.println("userRepository.findCardNumber() = " + userRepository.findCardNumber());
     }
 }
