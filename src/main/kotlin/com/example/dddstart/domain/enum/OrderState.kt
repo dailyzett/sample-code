6724f1141f403a093bf9eade4819cb982b2e476b
dailyzett <dailyzett@gmail.com>
2023-07-25 23:25:00 +0900

    주문 상태 enum class 정의

---
+++ src/main/kotlin/com/example/dddstart/domain/enum/OrderState.kt
@@ -0,0 +1,30 @@
+package com.example.dddstart.domain.enum
+
+/**
+ * 주문 상태
+ * - PAYMENT_WAITING: 결제 대기
+ * - PREPARING: 상품 준비
+ * - SHIPPED: 배송 중
+ * - DELIVERING: 배송 중
+ * - DELIVERY_COMPLETE: 배송 완료
+ * 결제 대기, 상품 준비 상태일 때는 배송지 정보를 변경할 수 있어야 한다.
+ */
+enum class OrderState {
+    PAYMENT_WAITING {
+        override fun isShippingChangeable(): Boolean {
+            return true
+        }
+    },
+
+    PREPARING {
+        override fun isShippingChangeable(): Boolean {
+            return true
+        }
+    },
+
+    SHIPPED, DELIVERING, DELIVERY_COMPLETE;
+
+    open fun isShippingChangeable(): Boolean {
+        return false
+    }
+}
