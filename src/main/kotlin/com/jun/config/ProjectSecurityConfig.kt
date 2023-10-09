b02b90d1bf7cc0ec7eb6f9da0ba765566826061d
sejun <dailyzett@gmail.com>
2023-10-10 02:01:40 +0900

    스프링 시큐리티 설정 수정

--- src/main/kotlin/com/jun/config/ProjectSecurityConfig.kt
+++ src/main/kotlin/com/jun/config/ProjectSecurityConfig.kt
@@ -1,15 +1,20 @@
 package com.jun.config
 
+import com.jun.filter.CsrfCookieFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configurers.*
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler
 import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -17,46 +22,42 @@ class ProjectSecurityConfig {
     @Bean
     @Throws(Exception::class)
     fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http
-            .authorizeHttpRequests {
-                it
-                    .requestMatchers("/myAccount/**", "/myBalance/**", "/myLoans/**", "/myCards/**", "/user")
-                    .authenticated()
+        val requestHandler = CsrfTokenRequestAttributeHandler()
+        requestHandler.setCsrfRequestAttributeName("_csrf")
+        http.securityContext { context: SecurityContextConfigurer<HttpSecurity?> ->
+            context
+                .requireExplicitSave(false)
+        }
+            .sessionManagement { session: SessionManagementConfigurer<HttpSecurity?> ->
+                session.sessionCreationPolicy(
+                    SessionCreationPolicy.ALWAYS
+                )
+            }
+            .cors { corsCustomizer: CorsConfigurer<HttpSecurity?> ->
+                corsCustomizer.configurationSource {
+                    val config = CorsConfiguration()
+                    config.allowedOrigins = listOf("http://localhost:4200")
+                    config.setAllowedMethods(listOf("*"))
+                    config.allowCredentials = true
+                    config.allowedHeaders = listOf("*")
+                    config.maxAge = 3600L
+                    return@configurationSource config
+                }
+            }.csrf { csrf: CsrfConfigurer<HttpSecurity?> ->
+                csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/contact", "/register")
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            }
+            .addFilterAfter(CsrfCookieFilter(), BasicAuthenticationFilter::class.java)
+            .authorizeHttpRequests { requests ->
+                requests
+                    .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards", "/user").authenticated()
                     .requestMatchers("/notices", "/contact", "/register").permitAll()
             }
-            .formLogin {
-                it
-                    .permitAll()
-            }
-            .httpBasic {
-                it
-                    .disable()
-            }
-            .csrf {
-                it
-                    .disable()
-            }
-            .cors {}
+            .formLogin(Customizer.withDefaults())
+            .httpBasic(Customizer.withDefaults())
         return http.build()
     }
 
-    @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource {
-        val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("http://localhost:4200")
-        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
-        configuration.allowCredentials = true
-        configuration.allowedHeaders = listOf("*")
-        configuration.maxAge = 3600L
-        val source = UrlBasedCorsConfigurationSource()
-        source.registerCorsConfiguration("/**", configuration)
-        return source
-    }
-
-    /*@Bean
-    fun userDetailService(dataSource: DataSource): JdbcUserDetailsManager {
-        return JdbcUserDetailsManager(dataSource)
-    }*/
 
     /**
      * NoOpPasswordEncoder is deprecated. Use BCryptPasswordEncoder instead.
@@ -65,34 +66,4 @@ class ProjectSecurityConfig {
     fun passwordEncoder(): PasswordEncoder {
         return BCryptPasswordEncoder()
     }
-
-    /*    @Bean
-        fun userDetamilService(): InMemoryUserDetailsManager {
-             InMemoryUserDetailsManager is deprecated. Use User.withDefaultPasswordEncoder() instead.
-            val admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("12345")
-                .roles("ADMIN")
-                .build()
-
-            val genericMember = User.withDefaultPasswordEncoder()
-                .username("genericMember")
-                .password("12345")
-                .roles("read")
-                .build()
-            return InMemoryUserDetailsManager(admin, genericMember)
-
-            val admin = User
-                .withUsername("admin")
-                .password("12345")
-                .roles("ADMIN")
-                .build()
-
-            val genericMember = User
-                .withUsername("genericMember")
-                .password("12345")
-                .roles("read")
-                .build()
-            return InMemoryUserDetailsManager(admin, genericMember)
-        }*/
 }
