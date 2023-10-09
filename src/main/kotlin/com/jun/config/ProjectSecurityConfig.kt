6b47afa97f92c4f6a74c256c895bf5e197638afc
sejun <dailyzett@gmail.com>
2023-10-09 20:05:19 +0900

    스프링 시큐리티 CORS 허용

--- src/main/kotlin/com/jun/config/ProjectSecurityConfig.kt
+++ src/main/kotlin/com/jun/config/ProjectSecurityConfig.kt
@@ -7,6 +7,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -15,25 +18,39 @@ class ProjectSecurityConfig {
     @Throws(Exception::class)
     fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
-            .authorizeHttpRequests { request ->
-                request
+            .authorizeHttpRequests {
+                it
                     .requestMatchers("/myAccount/**", "/myBalance/**", "/myLoans/**", "/myCards/**", "/user")
                     .authenticated()
                     .requestMatchers("/notices", "/contact", "/register").permitAll()
             }
-            .formLogin { formLogin ->
-                formLogin
+            .formLogin {
+                it
                     .permitAll()
             }
-            .httpBasic { httpBasic ->
-                httpBasic
+            .httpBasic {
+                it
                     .disable()
             }
-            .csrf { csrf ->
-                csrf
+            .csrf {
+                it
                     .disable()
             }
+            .cors {}
         return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("http://localhost:4200")
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
+        configuration.allowCredentials = true
+        configuration.allowedHeaders = listOf("*")
+        configuration.maxAge = 3600L
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 
     /*@Bean
