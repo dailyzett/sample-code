ac1904521d53db4d97ad06212bad1cf27dcfb48d
dailyzett <dailyzett@gmail.com>
2024-03-24 01:37:18 +0900

    feat: 일주일동안 특정 날짜에 연속으로 출석한 회원 수를 반환하는 부분 개발

--- src/main/kotlin/org/example/redisexample/controller/members/MainController.kt
+++ src/main/kotlin/org/example/redisexample/controller/members/MainController.kt
@@ -1,17 +1,24 @@
 package org.example.redisexample.controller.members
 
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.example.redisexample.service.attendance.AttendanceService
 import org.example.redisexample.service.stat.DauService
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
 
 @RestController
 @RequestMapping("/main")
 class MainController(
     private val dauService: DauService,
+    private val attendanceService: AttendanceService,
 ) {
+
+    private val log = KotlinLogging.logger { }
 
     @GetMapping
     fun mainDetails(@RequestParam userId: Int) {
@@ -22,5 +29,15 @@ class MainController(
     fun dauDetails(): ResponseEntity<Long> {
         val dauCount = dauService.getDauCount()
         return ResponseEntity.ok(dauCount)
+    }
+
+    @GetMapping("/attendance")
+    fun attendanceDetails(
+        @RequestParam @DateTimeFormat(pattern = "yyyyMMdd") startDate: LocalDate,
+        @RequestParam periodDay: Int
+    ): ResponseEntity<List<Int>> {
+        log.info { "startDate: $startDate periodDay: $periodDay" }
+        val res = attendanceService.findContinuousCheckedInWeeks(startDate, periodDay)
+        return ResponseEntity.ok(res)
     }
 }
--- src/main/kotlin/org/example/redisexample/service/attendance/AttendanceService.kt
+++ src/main/kotlin/org/example/redisexample/service/attendance/AttendanceService.kt
@@ -2,9 +2,11 @@ package org.example.redisexample.service.attendance
 
 import org.example.redisexample.util.KeyGenerator
 import org.example.redisexample.util.getDaysBefore
+import org.springframework.data.redis.connection.RedisStringCommands.BitOperation
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Service
 import java.time.Clock
+import java.time.LocalDate
 
 @Service
 class AttendanceService(
@@ -16,5 +18,38 @@ class AttendanceService(
         val uvKeyList = dayList.map { KeyGenerator.generateUserAttendanceKey(it) }
         uvKeyList.forEach { stringRedisTemplate.opsForValue().setBit(it, userID.toLong(), true) }
         return uvKeyList
+    }
+
+    /**
+     * 일주일동안 특정 기간 연속 출석 체크한 userId 를 찾습니다. 특정 기간의 시작점은 startDate 로부터 startDate + periodDay 까지 입니다.
+     * @param startDate 시작일
+     * @param periodDay 기간(일 단위)
+     */
+    fun findContinuousCheckedInWeeks(startDate: LocalDate, periodDay: Int): List<Int> {
+        val attendanceDates =
+            List(periodDay) { "uv:${changeDateToString(startDate.plusDays(it.toLong()))}".toByteArray() }
+
+        val key = "event:${changeDateToString(startDate).dropLast(2)}"
+        stringRedisTemplate.execute { connection ->
+            connection.stringCommands()
+                .bitOp(BitOperation.AND, key.toByteArray(), *attendanceDates.toTypedArray())
+        }
+
+        val attendanceResult = stringRedisTemplate.opsForValue().get(key) ?: ""
+        val attendanceIntList = convertBitToStringList(attendanceResult)
+        return attendanceIntList.withIndex().filter { it.value == 1 }.map { it.index }
+    }
+
+    private fun changeDateToString(date: LocalDate): String {
+        return date.toString().replace("-", "")
+    }
+
+    private fun convertBitToStringList(result: String): List<Int> {
+        val bitsList = mutableListOf<Int>()
+        result.forEach { char ->
+            val bits = (7 downTo 0).map { (char.code shr it) and 1 }
+            bitsList.addAll(bits)
+        }
+        return bitsList
     }
 }
