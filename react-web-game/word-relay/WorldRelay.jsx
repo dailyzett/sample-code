d2ae597e6dd695bc45fdba338bc9c8e5b5814da2
dailyzett <dailyzett@gmail.com>
2024-06-07 01:03:33 +0900

    feat: 끝말잇기 게임 훅 구현

--- react-web-game/word-relay/WorldRelay.jsx
+++ react-web-game/word-relay/WorldRelay.jsx
@@ -1,10 +1,40 @@
 const React = require('react');
+const {createRef} = require("react");
 
 function WorldRelay() {
+    const [prev, setPrev] = React.useState('정발산');
+    const [next, setNext] = React.useState('');
+    const [result, setResult] = React.useState('정발산');
+    const inputRef = createRef();
+
+    function handleChange(e) {
+        setNext(e.target.value);
+    }
+
+    function handleClick() {
+        const lastChar = prev[prev.length - 1];
+        const firstChar = next.at(0);
+
+        if (lastChar === firstChar) {
+            setPrev(next);
+            setResult(next);
+        }
+        setNext('');
+        inputRef.current.focus();
+    }
+
+    function handleKeyPress(e) {
+        if (e.key === 'Enter') {
+            handleClick();
+        }
+    }
+
     return (
-        <div>
-            "World Relay"
-        </div>
+        <>
+            <div>{result}</div>
+            <input value={next} onChange={handleChange} onKeyPress={handleKeyPress} ref={inputRef}/>
+            <button onClick={handleClick}>입력</button>
+        </>
     )
 }
 
--- react-web-game/word-relay/client.jsx
+++ react-web-game/word-relay/client.jsx
@@ -1,5 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const WorldRelay = require('./WorldRelay')
+const {createRoot} = require("react-dom/client");
 
-ReactDOM.render(<WorldRelay/>, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(<WorldRelay/>)
