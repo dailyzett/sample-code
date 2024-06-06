457603322f2bf49918e926ffbce83cbd4c1b5c17
dailyzett <dailyzett@gmail.com>
2024-06-06 22:45:59 +0900

    feat: 웹팩, 바벨 적용

---
+++ react-web-game/word-relay/WorldRelay.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+function WorldRelay() {
+    return (
+        <div>
+            "World Relay"
+        </div>
+    )
+}
+
+module.exports = WorldRelay;
---
+++ react-web-game/word-relay/client.jsx
@@ -0,0 +1,5 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const WorldRelay = require('./WorldRelay')
+
+ReactDOM.render(<WorldRelay/>, document.getElementById('root'));
