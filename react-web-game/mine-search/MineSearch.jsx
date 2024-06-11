35d9f30a2d86287dc1c43f993059a60afc71d07a
dailyzett <dailyzett@gmail.com>
2024-06-11 21:41:33 +0900

    지뢰찾기 게임 최초 커밋

---
+++ react-web-game/mine-search/MineSearch.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MineSearch() {
+    return (
+        <>
+            <div>지뢰찾기</div>
+        </>
+    )
+}
+
+export default MineSearch;
---
+++ react-web-game/mine-search/client.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import MineSearch from './MineSearch';
+import {createRoot} from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<MineSearch/>)
