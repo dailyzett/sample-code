56a4fb07d3c1cb957610f94ffeb550a9418dc104
dailyzett <dailyzett@gmail.com>
2024-06-13 23:20:28 +0900

    useContext API 최적화

--- react-web-game/mine-search/MineSearch.jsx
+++ react-web-game/mine-search/MineSearch.jsx
@@ -1,4 +1,4 @@
-import React, {createContext, useMemo, useReducer} from 'react';
+import React, {createContext, useEffect, useMemo, useReducer} from 'react';
 import Table from "./Table";
 import Form from "./Form";
 
@@ -22,9 +22,15 @@ export const TableContext = createContext({
 
 const initialState = {
     tableData: [],
+    data: {
+        row: 0,
+        cell: 0,
+        mine: 0,
+    },
     timer: 0,
     result: 0,
     halted: true,
+    openedCount: 0,
 }
 
 const plantMine = (row, cell, mine) => {
@@ -61,26 +67,46 @@ export const OPEN_CELL = 'OPEN_CELL';
 export const CLICK_MINE = 'CLICK_MINE';
 export const FLAG_CELL = 'FLAG_CELL';
 export const QUESTION_CELL = 'QUESTION_CELL';
-export const NORMALIZED_CELL = 'NORMALIZED_CELL'
+export const NORMALIZED_CELL = 'NORMALIZED_CELL';
+export const INCREMENT_TIMER = 'INCREMENT_TIMER';
 
 const reducer = (state, action) => {
     switch (action.type) {
         case START_GAME:
             return {
                 ...state,
+                data: {
+                    row: action.row,
+                    cell: action.cell,
+                    mine: action.mine,
+                },
                 tableData: plantMine(action.row, action.cell, action.mine),
-                halted: false
+                halted: false,
+                openedCount: 0,
+                timer: 0,
             };
         case OPEN_CELL: {
             const tableData = [...state.tableData];
             tableData[action.row] = [...state.tableData[action.row]];
             tableData[action.row][action.cell] = CODE.OPENED;
-            const checked = [];
 
+            tableData.forEach((row, i) => {
+                tableData[i] = [...state.tableData[i]];
+            });
+            const checked = [];
+            let counter = 0;
             const checkAround = (row, cell) => {
                 if (row < 0 || row >= tableData.length || cell < 0 || cell >= tableData[0].length) {
                     return;
                 } // 상하좌우 없는칸은 안 열기
+                if ([CODE.OPENED, CODE.FLAG, CODE.FLAG_MINE, CODE.QUESTION_MINE, CODE.QUESTION].includes(tableData[row][cell])) {
+                    return;
+                }
+                if (checked.includes(row + '/' + cell)) {
+                    return;
+                } else {
+                    checked.push(row + '/' + cell);
+                }
                 let around = [
                     tableData[row][cell - 1], tableData[row][cell + 1],
                 ];
@@ -90,17 +116,49 @@ const reducer = (state, action) => {
                 if (tableData[row + 1]) {
                     around = around.concat([tableData[row + 1][cell - 1], tableData[row + 1][cell], tableData[row + 1][cell + 1]]);
                 }
-
                 const count = around.filter(function (v) {
                     return [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v);
                 }).length;
+                if (count === 0) {
+                    const near = [];
+                    if (row - 1 > 1) {
+                        near.push([row - 1, cell - 1]);
+                        near.push([row - 1, cell]);
+                        near.push([row - 1, cell + 1]);
+                    }
+                    near.push([row, cell - 1]);
+                    near.push([row, cell + 1]);
+                    if (row + 1 < tableData.length) {
+                        near.push([row + 1, cell - 1]);
+                        near.push([row + 1, cell]);
+                        near.push([row + 1, cell + 1]);
+                    }
+                    near.forEach((n) => {
+                        if (tableData[n[0]][n[1]] !== CODE.OPENED) {
+                            checkAround(n[0], n[1]);
+                        }
+                    });
+                }
+                if (tableData[row][cell] === CODE.NORMAL) {
+                    counter += 1;
+                }
                 tableData[row][cell] = count;
             }
             checkAround(action.row, action.cell);
+            let halted = false;
+            let result = '';
+            console.log(state.data.row * state.data.cell - state.data.mine, state.openedCount + counter);
+            if (state.data.row * state.data.cell - state.data.mine <= state.openedCount + counter) {
+                halted = true;
+                result = `${state.timer}초 만에 승리하셨습니다`;
+            }
             return {
                 ...state,
                 tableData,
-            }
+                openedCount: state.openedCount + counter,
+                halted,
+                result,
+            };
         }
         case CLICK_MINE: {
             const tableData = [...state.tableData];
@@ -154,17 +212,35 @@ const reducer = (state, action) => {
                 tableData
             }
         }
+        case INCREMENT_TIMER: {
+            return {
+                ...state,
+                timer: state.timer + 1
+            }
+        }
         default:
             return state;
     }
 };
 
-function MineSearch() {
+const MineSearch = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
     const {timer, result, tableData, halted} = state;
     const value = useMemo(() => (
         {tableData: tableData, halted: halted, dispatch}
     ), [tableData, halted]);
+
+    useEffect(() => {
+        let timer;
+        if (halted === false) {
+            timer = setInterval(() => {
+                dispatch({type: INCREMENT_TIMER});
+            }, 1000);
+        }
+        return () => {
+            clearInterval(timer);
+        }
+    }, [halted]);
 
     return (
         <TableContext.Provider value={value}>
--- react-web-game/mine-search/Td.jsx
+++ react-web-game/mine-search/Td.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useContext} from 'react';
+import React, {memo, useCallback, useContext, useMemo} from 'react';
 import {CLICK_MINE, CODE, FLAG_CELL, NORMALIZED_CELL, OPEN_CELL, QUESTION_CELL, TableContext} from "./MineSearch";
 
 
@@ -39,7 +39,7 @@ const getTdText = (code) => {
     }
 }
 
-const Td = ({rowIndex, cellIndex}) => {
+const Td = memo(({rowIndex, cellIndex}) => {
     const {tableData, dispatch, halted} = useContext(TableContext)
 
     const onClickTd = useCallback(() => {
@@ -82,13 +82,13 @@ const Td = ({rowIndex, cellIndex}) => {
         }
     }, [tableData[rowIndex][cellIndex], halted]);
 
-    return (
+    return useMemo(() => (
         <td style={getTdStyle(tableData[rowIndex][cellIndex])}
             onClick={onClickTd}
             onContextMenu={onRightClickTd}>
             {getTdText(tableData[rowIndex][cellIndex])}
         </td>
-    )
-}
+    ), [tableData[rowIndex][cellIndex]])
+});
 
 export default Td;
