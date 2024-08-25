d1a330264a285c6dea87ed029c3648153b65c77a
김세준 <dailyzett@gmail.com>
2024-08-25 23:22:32 +0900

    feat: 최초 커밋

---
+++ src/test/kotlin/org/example/eventsourcing/EventSourcingApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.eventsourcing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EventSourcingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
